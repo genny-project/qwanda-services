6f68bb0c5436e291f9c307ab09c975b776b3bfae
andres <helios@andress-MBP.lan>
2017-11-14 16:19:33 +1100

    baseentityservice new constructor for postconstruct

--- src/main/java/life/genny/daoservices/BaseEntityService.java
+++ src/main/java/life/genny/daoservices/BaseEntityService.java
@@ -7,13 +7,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.InvocationTargetException;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.TimeZone;
 import java.util.stream.Collectors;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Event;
@@ -27,6 +31,7 @@ import javax.ws.rs.core.MultivaluedMap;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.exception.ConstraintViolationException;
+import org.jboss.resteasy.specimpl.MultivaluedMapImpl;
 import org.keycloak.KeycloakSecurityContext;
 import org.mortbay.log.Log;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -72,6 +77,9 @@ public class BaseEntityService {
   // @Inject
   // SecurityService securityService;
 
+//  @Inject
+//  private PersistenceHelper helper;
+  
   @Inject
   private Event<BaseEntity> baseEntityEventSrc;
 
@@ -92,6 +100,10 @@ public class BaseEntityService {
 
   EntityManager em;
 
+  protected BaseEntityService() {
+    
+  }
+  
   public BaseEntityService(final EntityManager em) {
     this.em = em;
   }
@@ -536,43 +548,43 @@ public class BaseEntityService {
   }
 
   public Ask findAskById(final Long id) {
-    return em.find(Ask.class, id);
+    return  em.find(Ask.class, id);
   }
 
   public GPS findGPSById(final Long id) {
-    return em.find(GPS.class, id);
+    return  em.find(GPS.class, id);
   }
 
   public Question findQuestionById(final Long id) {
-    return em.find(Question.class, id);
+    return  em.find(Question.class, id);
   }
 
   public Answer findAnswerById(final Long id) {
-    return em.find(Answer.class, id);
+    return  em.find(Answer.class, id);
   }
 
   public life.genny.qwanda.Context findContextById(final Long id) {
-    return em.find(life.genny.qwanda.Context.class, id);
+    return  em.find(life.genny.qwanda.Context.class, id);
   }
 
   public BaseEntity findBaseEntityById(final Long id) {
-    return em.find(BaseEntity.class, id);
+    return  em.find(BaseEntity.class, id);
   }
 
   public Attribute findAttributeById(final Long id) {
-    return em.find(Attribute.class, id);
+    return  em.find(Attribute.class, id);
   }
 
   public Rule findRuleById(final Long id) {
-    return em.find(Rule.class, id);
+    return  em.find(Rule.class, id);
   }
 
   public Validation findValidationById(final Long id) {
-    return em.find(Validation.class, id);
+    return  em.find(Validation.class, id);
   }
 
   public DataType findDataTypeById(final Long id) {
-    return em.find(DataType.class, id);
+    return  em.find(DataType.class, id);
   }
 
   
@@ -892,50 +904,50 @@ public class BaseEntityService {
     return total;
   }
 
-  // public Setup setup(final KeycloakSecurityContext kContext) {
-  // final Setup setup = new Setup();
-  // String bearerToken = null;
-  // String decodedJson = null;
-  // JSONObject jsonObj;
-  // try {
-  // bearerToken = kContext.getTokenString();
-  // System.out.println("bearerToken:" + bearerToken);
-  // final String[] jwtToken = bearerToken.split("\\.");
-  // System.out.println("jwtToken:" + jwtToken);
-  // final Decoder decoder = Base64.getDecoder();
-  // final byte[] decodedClaims = decoder.decode(jwtToken[1]);
-  // decodedJson = new String(decodedClaims);
-  // System.out.println("decodedJson:" + decodedJson);
-  // jsonObj = new JSONObject(decodedJson);
-  // final String userUUID = jsonObj.getString("sub");
-  // System.out.println("UserId=" + userUUID);
-  // final JSONObject realm_access = (JSONObject) jsonObj.get("realm_access");
-  // final JSONArray realm_roles = (JSONArray) realm_access.get("roles");
-  // final JSONObject resource_access = (JSONObject) jsonObj.get("resource_access");
-  // final JSONObject qwandaService = (JSONObject) resource_access.get("qwanda-service");
-  // final JSONArray resource_roles = (JSONArray) qwandaService.get("roles");
-  //
-  // System.out.println("Roles:" + resource_roles + "," + realm_roles + "!");
-  //
-  // final BaseEntity be =
-  // findUserByAttributeValue(AttributeText.getDefaultCodePrefix() + "UUID", userUUID);
-  // be.getBaseEntityAttributes();
-  // setup.setUser(be);
-  //
-  // //
-  // {"jti":"1ae163f0-5495-4466-b224-de35a1f5794b","exp":1494376724,"nbf":0,"iat":1494376424,"iss":"http://bouncer.outcome-hub.com/auth/realms/genny","aud":"qwanda-service","sub":"81ef02bd-9976-4ce4-9fb4-17f30b416e06","typ":"Bearer","azp":"qwanda-service","auth_time":1494376102,"session_state":"4153d350-e9e9-4a00-85de-2def89427f4e","acr":"0","client_session":"307f1c32-c7ea-4408-816c-12a189c09081","allowed-origins":["*"],"realm_access":{"roles":["uma_authorization","user"]},"resource_access":{"qwanda-service":{"roles":["admin"]},"account":{"roles":["manage-account","manage-account-links","view-profile"]}},"name":"Bob
-  // //
-  // Console","preferred_username":"adamcrow63+bobconsole@gmail.com","given_name":"Bob","family_name":"Console","email":"adamcrow63+bobconsole@gmail.com"}
-  // } catch (final JSONException e1) {
-  // // log.error("bearerToken=" + bearerToken + " decodedJson=" + decodedJson + ":"
-  // // +
-  // // e1.getMessage());
-  // }
-  //
-  // setup.setLayout("layout1");
-  //
-  // return setup;
-  // }
+//   public Setup setup(final KeycloakSecurityContext kContext) {
+//   final Setup setup = new Setup();
+//   String bearerToken = null;
+//   String decodedJson = null;
+//   JSONObject jsonObj;
+//   try {
+//   bearerToken = kContext.getTokenString();
+//   System.out.println("bearerToken:" + bearerToken);
+//   final String[] jwtToken = bearerToken.split("\\.");
+//   System.out.println("jwtToken:" + jwtToken);
+//   final Decoder decoder = Base64.getDecoder();
+//   final byte[] decodedClaims = decoder.decode(jwtToken[1]);
+//   decodedJson = new String(decodedClaims);
+//   System.out.println("decodedJson:" + decodedJson);
+//   jsonObj = new JSONObject(decodedJson);
+//   final String userUUID = jsonObj.getString("sub");
+//   System.out.println("UserId=" + userUUID);
+//   final JSONObject realm_access = (JSONObject) jsonObj.get("realm_access");
+//   final JSONArray realm_roles = (JSONArray) realm_access.get("roles");
+//   final JSONObject resource_access = (JSONObject) jsonObj.get("resource_access");
+//   final JSONObject qwandaService = (JSONObject) resource_access.get("qwanda-service");
+//   final JSONArray resource_roles = (JSONArray) qwandaService.get("roles");
+//  
+//   System.out.println("Roles:" + resource_roles + "," + realm_roles + "!");
+//  
+//   final BaseEntity be =
+//   findUserByAttributeValue(AttributeText.getDefaultCodePrefix() + "UUID", userUUID);
+//   be.getBaseEntityAttributes();
+//   setup.setUser(be);
+//  
+//   //
+//   {"jti":"1ae163f0-5495-4466-b224-de35a1f5794b","exp":1494376724,"nbf":0,"iat":1494376424,"iss":"http://bouncer.outcome-hub.com/auth/realms/genny","aud":"qwanda-service","sub":"81ef02bd-9976-4ce4-9fb4-17f30b416e06","typ":"Bearer","azp":"qwanda-service","auth_time":1494376102,"session_state":"4153d350-e9e9-4a00-85de-2def89427f4e","acr":"0","client_session":"307f1c32-c7ea-4408-816c-12a189c09081","allowed-origins":["*"],"realm_access":{"roles":["uma_authorization","user"]},"resource_access":{"qwanda-service":{"roles":["admin"]},"account":{"roles":["manage-account","manage-account-links","view-profile"]}},"name":"Bob
+//   //
+//   Console","preferred_username":"adamcrow63+bobconsole@gmail.com","given_name":"Bob","family_name":"Console","email":"adamcrow63+bobconsole@gmail.com"}
+//   } catch (final JSONException e1) {
+//   // log.error("bearerToken=" + bearerToken + " decodedJson=" + decodedJson + ":"
+//   // +
+//   // e1.getMessage());
+//   }
+//  
+//   setup.setLayout("layout1");
+//  
+//   return setup;
+//   }
 
   // public void importKeycloakUsers(final List<Group> parentGroupList,
   // final AttributeLink linkAttribute, final Integer maxReturned)
@@ -1591,120 +1603,134 @@ public class BaseEntityService {
     return beMap.values().stream().collect(Collectors.toList());
   }
 
-  // public Long importKeycloakUsers(final String keycloakUrl, final String realm,
-  // final String username, final String password, final String clientId,
-  // final Integer maxReturned, final String parentGroupCodes) {
-  // Long count = 0L;
-  //
-  // AttributeLink linkAttribute = this.findAttributeLinkByCode("LNK_CORE");
-  // List<BaseEntity> parentGroupList = new ArrayList<BaseEntity>();
-  //
-  // String[] parentCodes = parentGroupCodes.split(",");
-  // for (String parentCode : parentCodes) {
-  // BaseEntity group = null;
-  //
-  // try {
-  // group = this.findBaseEntityByCode(parentCode); // careful as GRPUSERS needs to
-  // parentGroupList.add(group);
-  // } catch (NoResultException e) {
-  // System.out.println("Group Code does not exist :" + parentCode);
-  // }
-  //
-  // }
+   public Long importKeycloakUsers(final String keycloakUrl, final String realm,
+   final String username, final String password, final String clientId,
+   final Integer maxReturned, final String parentGroupCodes) {
+   Long count = 0L;
+  
+   AttributeLink linkAttribute = this.findAttributeLinkByCode("LNK_CORE");
+   List<BaseEntity> parentGroupList = new ArrayList<BaseEntity>();
+  
+   String[] parentCodes = parentGroupCodes.split(",");
+   for (String parentCode : parentCodes) {
+   BaseEntity group = null;
+  
+   try {
+   group = this.findBaseEntityByCode(parentCode); // careful as GRPUSERS needs to
+   parentGroupList.add(group);
+   } catch (NoResultException e) {
+   System.out.println("Group Code does not exist :" + parentCode);
+   }
+  
+   }
 
-  // KeycloakService ks;
-  // final Map<String, Map<String, Object>> usersMap = new HashMap<String, Map<String, Object>>();
-  //
-  // try {
-  // ks = new KeycloakService(keycloakUrl, realm, username, password, clientId);
-  // final List<LinkedHashMap> users = ks.fetchKeycloakUsers(maxReturned);
-  // for (final Object user : users) {
-  // final LinkedHashMap map = (LinkedHashMap) user;
-  // final Map<String, Object> userMap = new HashMap<String, Object>();
-  // for (final Object key : map.keySet()) {
-  // // System.out.println(key + ":" + map.get(key));
-  // userMap.put((String) key, map.get(key));
-  //
-  // }
-  // usersMap.put((String) userMap.get("username"), userMap);
-  // System.out.println();
-  // }
-  //
-  // System.out.println("finished");
-  // } catch (final IOException e) {
-  // // TODO Auto-generated catch block
-  // e.printStackTrace();
-  // }
-  //
-  // for (final String kcusername : usersMap.keySet()) {
-  // final MultivaluedMap params = new MultivaluedMapImpl();
-  // params.add("PRI_USERNAME", kcusername);
-  // final Map<String, Object> userMap = usersMap.get(kcusername);
-  //
-  // final List<BaseEntity> users = findBaseEntitysByAttributeValues(params, true, 0, 1);
-  // if (users.isEmpty()) {
-  // final String code = "PER_CH40_" + kcusername.toUpperCase().replaceAll("\\ ", "")
-  // .replaceAll("\\.", "").replaceAll("\\&", "");
-  // System.out.println("New User Code = " + code);
-  // String firstName = (String) userMap.get("firstName");
-  // firstName = firstName.replaceAll("\\.", " "); // replace dots
-  // firstName = firstName.replaceAll("\\_", " "); // replace dots
-  // String lastName = (String) userMap.get("lastName");
-  // lastName = lastName.replaceAll("\\.", " "); // replace dots
-  // lastName = lastName.replaceAll("\\_", " "); // replace dots
-  // String name = firstName + " " + lastName;
-  //
-  // final String email = (String) userMap.get("email");
-  // final String id = (String) userMap.get("id");
-  // final Long unixSeconds = (Long) userMap.get("createdTimestamp");
-  // final Date date = new Date(unixSeconds); // *1000 is to convert seconds to milliseconds
-  // final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z"); // the format of
-  // // your date
-  // sdf.setTimeZone(TimeZone.getTimeZone("GMT+10")); // give a timezone reference for formating
-  // sdf.format(date);
-  // final Attribute firstNameAtt = findAttributeByCode("PRI_FIRSTNAME");
-  // final Attribute lastNameAtt = findAttributeByCode("PRI_LASTNAME");
-  // final Attribute nameAtt = findAttributeByCode("PRI_NAME");
-  // final Attribute emailAtt = findAttributeByCode("PRI_EMAIL");
-  // final Attribute uuidAtt = findAttributeByCode("PRI_UUID");
-  // final Attribute usernameAtt = findAttributeByCode("PRI_USERNAME");
-  //
-  // try {
-  // final BaseEntity user = new BaseEntity(code, name);
-  //
-  // user.addAttribute(firstNameAtt, 0.0, firstName);
-  // user.addAttribute(lastNameAtt, 0.0, lastName);
-  // user.addAttribute(nameAtt, 0.0, name);
-  // user.addAttribute(emailAtt, 0.0, email);
-  // user.addAttribute(uuidAtt, 0.0, id);
-  // user.addAttribute(usernameAtt, 0.0, kcusername);
-  // insert(user);
-  //
-  // // Now link to groups
-  // for (final BaseEntity parent : parentGroupList) {
-  // if (!parent.containsTarget(user.getCode(), linkAttribute.getCode())) {
-  // parent.addTarget(user, linkAttribute, 1.0);
-  // }
-  // }
-  // count++;
-  // System.out.println("BE:" + user);
-  // } catch (final BadDataException e) {
-  // // TODO Auto-generated catch block
-  // e.printStackTrace();
-  // }
-  //
-  // } else {
-  // users.get(0);
-  // }
-  //
-  // }
-  //
-  // // now save the parents
-  // for (BaseEntity parent : parentGroupList) {
-  // parent = em.merge(parent);
-  // }
-  //
-  // return count;
-  // }
+   KeycloakService ks;
+   final Map<String, Map<String, Object>> usersMap = new HashMap<String, Map<String, Object>>();
+  
+   try {
+   ks = new KeycloakService(keycloakUrl, realm, username, password, clientId);
+   final List<LinkedHashMap> users = ks.fetchKeycloakUsers(maxReturned);
+   for (final Object user : users) {
+   final LinkedHashMap map = (LinkedHashMap) user;
+   final Map<String, Object> userMap = new HashMap<String, Object>();
+   for (final Object key : map.keySet()) {
+   // System.out.println(key + ":" + map.get(key));
+   userMap.put((String) key, map.get(key));
+  
+   }
+   usersMap.put((String) userMap.get("username"), userMap);
+   System.out.println();
+   }
+  
+   System.out.println("finished");
+   } catch (final IOException e) {
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+   }
+  
+   for (final String kcusername : usersMap.keySet()) {
+   final MultivaluedMap params = new MultivaluedMapImpl();
+   params.add("PRI_USERNAME", kcusername);
+   final Map<String, Object> userMap = usersMap.get(kcusername);
+  
+   final List<BaseEntity> users = findBaseEntitysByAttributeValues(params, true, 0, 1);
+   if (users.isEmpty()) {
+   final String code = "PER_CH40_" + kcusername.toUpperCase().replaceAll("\\ ", "")
+   .replaceAll("\\.", "").replaceAll("\\&", "");
+   System.out.println("New User Code = " + code);
+   String firstName = (String) userMap.get("firstName");
+   firstName = firstName.replaceAll("\\.", " "); // replace dots
+   firstName = firstName.replaceAll("\\_", " "); // replace dots
+   String lastName = (String) userMap.get("lastName");
+   lastName = lastName.replaceAll("\\.", " "); // replace dots
+   lastName = lastName.replaceAll("\\_", " "); // replace dots
+   String name = firstName + " " + lastName;
+  
+   final String email = (String) userMap.get("email");
+   final String id = (String) userMap.get("id");
+   final Long unixSeconds = (Long) userMap.get("createdTimestamp");
+   final Date date = new Date(unixSeconds); // *1000 is to convert seconds to milliseconds
+   final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z"); // the format of
+   // your date
+   sdf.setTimeZone(TimeZone.getTimeZone("GMT+10")); // give a timezone reference for formating
+   sdf.format(date);
+   final Attribute firstNameAtt = findAttributeByCode("PRI_FIRSTNAME");
+   final Attribute lastNameAtt = findAttributeByCode("PRI_LASTNAME");
+   final Attribute nameAtt = findAttributeByCode("PRI_NAME");
+   final Attribute emailAtt = findAttributeByCode("PRI_EMAIL");
+   final Attribute uuidAtt = findAttributeByCode("PRI_UUID");
+   final Attribute usernameAtt = findAttributeByCode("PRI_USERNAME");
+  
+   try {
+   final BaseEntity user = new BaseEntity(code, name);
+  
+   user.addAttribute(firstNameAtt, 0.0, firstName);
+   user.addAttribute(lastNameAtt, 0.0, lastName);
+   user.addAttribute(nameAtt, 0.0, name);
+   user.addAttribute(emailAtt, 0.0, email);
+   user.addAttribute(uuidAtt, 0.0, id);
+   user.addAttribute(usernameAtt, 0.0, kcusername);
+   insert(user);
+  
+   // Now link to groups
+   for (final BaseEntity parent : parentGroupList) {
+   if (!parent.containsTarget(user.getCode(), linkAttribute.getCode())) {
+   parent.addTarget(user, linkAttribute, 1.0);
+   }
+   }
+   count++;
+   System.out.println("BE:" + user);
+   } catch (final BadDataException e) {
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+   }
+  
+   } else {
+   users.get(0);
+   }
+  
+   }
+  
+   // now save the parents
+   for (BaseEntity parent : parentGroupList) {
+   parent = em.merge(parent);
+   }
+  
+   return count;
+   }
+
+  /**
+   * @return the em
+   */
+  public EntityManager getEm() {
+    return em;
+  }
+
+  /**
+   * @param em the em to set
+   */
+  public void setEm(EntityManager em) {
+    this.em = em;
+  }
 
 }
