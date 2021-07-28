9f89289852ed57ffd965665284bb3f6d43417f8e
Jasper Robison <jasper.robison22@gmail.com>
2021-07-28 21:43:38 +1000

    try wothout ordinal

--- src/main/java/life/genny/services/BaseEntityService2.java
+++ src/main/java/life/genny/services/BaseEntityService2.java
@@ -415,7 +415,7 @@ public class BaseEntityService2 {
 		}
 		// Add BaseEntity Status expression
 		// builder.and(baseEntity.status.coalesce(EEntityStatus.ACTIVE).asEnum().ordinal().loe(status.ordinal()));
-		// builder.and(baseEntity.status.ordinal().loe(status.ordinal()));
+		builder.and(baseEntity.status.lt(status));
 		// Order the sorts by weight
 		Comparator<EntityAttribute> compareByWeight = (EntityAttribute a, EntityAttribute b) -> a.getWeight().compareTo(b.getWeight());
 		Collections.sort(sortAttributes, compareByWeight);
